1fe42c945260a56625806505bb79c5d64cf24e28
linln1 <19801335167@163.com>
2020-11-29 21:52:36 +0800

    add pyhw6

---
+++ pyhw6/gui.py
@@ -0,0 +1,226 @@
+import datetime
+import os
+import sys
+import logging
+from time import sleep
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtNetwork import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebSockets import *
+import humanfriendly
+import traceback
+
+
+class WindowClass(QWidget):
+
+    def __init__(self,parent=None):
+
+        super(WindowClass, self).__init__(parent)
+        layout=QVBoxLayout()
+
+        self.widgetstack = []
+
+        self.startBtn = QPushButton()
+        self.startBtn.setText("start")
+        self.startBtn.clicked.connect(self.onclickStart)
+        self.startBtn.setFixedSize(90, 150)
+
+        self.exitBtn = QPushButton()
+        self.exitBtn.clicked.connect(self.onclickExit)
+        self.exitBtn.setText("exit")
+        self.exitBtn.setFixedSize(90, 150)
+
+        # self.btn=QPushButton()
+        # self.btn.setText("显示对话框")
+        # self.btn.clicked.connect(self.showDialog)
+        self.resize(500,500)
+        #layout.addWidget(self.btn)
+        layout.addWidget(self.startBtn)
+        layout.addWidget(self.exitBtn)
+        layout.setAlignment(Qt.AlignCenter)
+        self.setLayout(layout)
+
+        self.processIdLine = QLineEdit()
+        self.bandWidth = QTextLine()
+        self.websocket = QWebSocket()
+        self.process = QProcess()
+        self.process.setProcessChannelMode(QProcess.MergedChannels)
+        self.process.finished.connect(self.processFinish)
+        self.process.started.connect(self.processStart)
+        self.process.readyReadStandardOutput.connect(self.getStatus)
+
+    def onclickStart(self):
+        w = QWidget()
+        self.widgetstack.append(w)
+        vbox = QVBoxLayout()  # 纵向布局
+        hbox = QHBoxLayout()  # 横向布局
+        vboxBtn = QVBoxLayout()
+        # self.startBtn.hide()
+        # self.exitBtn.hide()
+
+        self.username = QLineEdit()
+        self.username.setPlaceholderText('input username : ')
+        self.password = QLineEdit()
+        self.password.setEchoMode(QLineEdit.Password)
+        self.password.setPlaceholderText('input password')
+
+        self.listenPort = QLineEdit()
+        self.listenPort.setPlaceholderText('listenPort : ')
+        self.listenHost = QLineEdit()
+        self.listenHost.setPlaceholderText('listenHost : ')
+        self.consolePort = QLineEdit()
+        self.consolePort.setText(str(8896))
+        self.remoteHost = QLineEdit()
+        self.remoteHost.setPlaceholderText('remoteHost :')
+        self.remotePort = QLineEdit()
+        self.remotePort.setPlaceholderText('remotePort :')
+
+        self.connectBtn = QPushButton()
+        self.connectBtn.setText('conncet')
+        self.connectBtn.clicked.connect(self.connect)
+        self.backBtn = QPushButton()
+        self.backBtn.setText('back')
+        self.backBtn.clicked.connect(self.back)
+
+        vbox.addWidget(self.username)
+        vbox.addWidget(self.password)
+        vbox.addWidget(self.listenHost)
+        vbox.addWidget(self.listenPort)
+        vbox.addWidget(self.remoteHost)
+        vbox.addWidget(self.remotePort)
+
+        vboxBtn.addWidget(self.connectBtn)
+        vboxBtn.addWidget(self.backBtn)
+
+        hbox.addLayout(vbox)
+        hbox.addLayout(vboxBtn)
+        w.setWindowModality(Qt.ApplicationModal)
+        w.setLayout(hbox)
+        w.show()
+
+    def onclickExit(self):
+        vbox = QVBoxLayout()
+        hbox = QHBoxLayout()
+        panel = QLabel()
+        panel.setText("确定退出？")
+        self.dialog = QDialog()
+        self.dialog.resize(100, 100)
+        self.okBtn = QPushButton("确定")
+        self.cancelBtn = QPushButton("取消")
+        self.okBtn.clicked.connect(self.ok)
+        self.cancelBtn.clicked.connect(self.cancel)
+        self.dialog.setWindowTitle("提示信息！")
+
+        hbox.addWidget(self.okBtn)
+        hbox.addWidget(self.cancelBtn)
+
+        # 消息label与按钮组合纵向布局
+        vbox.addWidget(panel)
+        vbox.addLayout(hbox)
+        self.dialog.setLayout(vbox)
+
+        self.dialog.setWindowModality(Qt.ApplicationModal)  # 该模式下，只有该dialog关闭，才可以关闭父界面
+        self.dialog.exec_()
+        pass
+
+    def ok(self):
+        print("确定退出！")
+        self.dialog.close()
+        self.close()
+    def cancel(self):
+        print("取消退出！")
+        self.dialog.close()
+    def back(self):
+        print("返回！")
+        self.widgetstack[-1].close()
+    def connect(self):
+        username = self.username.text()
+        password = self.password.text()
+        listenHost = self.listenHost.text()
+        listenPort = self.listenPort.text()
+        consolePort = self.consolePort.text()
+        remoteHost = self.remoteHost.text()
+        remotePort = self.remotePort.text()
+        pythonExec = os.path.basename(sys.executable)
+        # 从localgui启动localproxy直接使用-w 提供用户密码，不再使用命令行交互输入，因为有些许问题
+        # _parser.add_argument('--proto', dest='proto', metavar='protocol', default='http tunnel')
+        # _parser.add_argument('--host', dest='listenHost', metavar='listen_host', default='127.0.0.1',
+        #                      help='proxy listen host default listen all interfaces')
+        # _parser.add_argument('--port', dest='listenPort', metavar='listen_port', default=8888, required=False,
+        #                      help='proxy listen port')
+        # _parser.add_argument('--username', dest='user', metavar='user', default='u1', help='username')
+        # _parser.add_argument('--password', dest='pwd', metavar='pwd', default='11', help='password')
+
+        cmdLine = f'{pythonExec} local.py --host={listenHost} --port={listenPort} --username={username} --password={password}'
+        log.debug(f'cmd={cmdLine}')
+        self.process.start(cmdLine)
+
+        self.process.kill()
+
+
+    def getStatus(self, traceback=None):
+        data = self.process.readAll()
+        try:
+            msg = data.data().decode().strip()
+            log.debug(f'msg={msg}')
+        except Exception as exc:
+            log.error(f'{traceback.format_exc()}')
+            exit(1)
+
+    def processStart(self):
+        process = self.sender()  # 此处等同于 self.process 只不过使用sender适应性更好
+        processId = process.processId()
+        log.debug(f'pid={processId}')
+        #self.startBtn.setText('Stop')
+        self.processIdLine.setText(str(processId))
+
+        self.websocket.connected.connect(self.websocketConnected)
+        self.websocket.disconnected.connect(self.websocketDisconnected)
+        self.websocket.textMessageReceived.connect(self.websocketMsgRcvd)
+        self.websocket.open(QUrl(f'ws://127.0.0.1:{self.consolePort.text()}/'))
+
+    def processFinish(self):
+        process = self.sender()
+        processId = process.processId()
+        log.debug(f'end process {processId}')
+        self.process.kill()
+        self.websocket.close()
+
+    def websocketMsgRcvd(self, msg):
+        if msg != None:
+            SuccessBtn = QPushButton()
+            SuccessBtn.setText("connect successfully")
+            sleep(1)
+            SuccessBtn.close()
+            self.widgetstack[-1].close()
+
+        log.debug(f'msg={msg}')
+        sendBandwidth , *_ = msg.split()
+        dt = datetime.datetime.now()
+        nowTime = dt.strftime('%Y-%m-%d %H:%M:%S ')
+        self.bandWidth.setText(f'{nowTime} {humanfriendly.format_size(int(sendBandwidth))}')
+
+    def websocketConnected(self):#以加密形式发送数据
+        self.websocket.sendTextMessage('secret')
+
+    def websocketDisconnected(self):
+        self.process.kill()
+
+
+if __name__=="__main__":
+
+    _logFmt = logging.Formatter('%(asctime)s %(levelname).1s %(lineno)-3d %(funcName)-20s %(message)s', datefmt='%H:%M:%S')
+    _consoleHandler = logging.StreamHandler()
+    _consoleHandler.setLevel(logging.DEBUG)
+    _consoleHandler.setFormatter(_logFmt)
+
+    log = logging.getLogger(__file__)
+    log.addHandler(_consoleHandler)
+    log.setLevel(logging.DEBUG)
+
+    app=QApplication(sys.argv)
+    win=WindowClass()
+    win.show()
+    sys.exit(app.exec_())
---
+++ pyhw6/remote.py
@@ -0,0 +1,377 @@
+
+import asyncio
+import ipaddress
+import logging
+import signal
+import struct
+import sqlite3
+import aiosqlite
+import argparse
+from time import time
+from threading import RLock, Timer
+import better_exceptions
+
+from enum import Enum
+ReadMode = Enum('ReadMod', ('EXACT', 'LINE', 'MAX', 'UNTIL'))
+
+remoteProxyHost = '127.0.0.1'
+remoteProxyPort = 8889
+remotesocksPort = 8890
+uBucketdict = dict()
+sBucketdict = dict()
+
+spareLock = asyncio.Lock()
+
+class MyError(Exception):
+    pass
+
+async def aioClose(w, *, logHint=None):
+    if not w:
+        await asyncio.sleep(0.001)
+        return
+    host, port, *_ = w.get_extra_info('peername')
+    log.info(f'{logHint} close... peer {host} {port}')
+    try:
+        w.close()
+        await w.wait_closed()
+    except Exception as exc:
+        pass
+
+async def aioRead( r, mode, *, logHint=None, exactData=None, exactLen=None, maxLen=-1, untilSep=b'\r\n', user = None, bucket = None):
+    data = None
+    uBucket = None
+    # if user:
+    #     uBucket = uBucketdict[user]
+    # if bucket:
+    #     uBucket = bucket
+    try:
+        if ReadMode.EXACT == mode:
+            exactLen = len(exactData) if exactData else exactLen
+            data = await r.readexactly(exactLen)
+            if exactData and data != exactData:
+                raise MyError(f'recvERR={data} {logHint}')
+        elif ReadMode.LINE == mode:
+            data = await r.readline()
+        elif ReadMode.MAX == mode:
+            data = await r.read(maxLen)
+        elif ReadMode.UNTIL == mode:
+            data = await r.readuntil(untilSep)
+        else:
+            log.error(f'INVALID mode={mode}')
+            exit(1)
+    except ConnectionResetError as exc:
+        raise MyError(f'recvEXC={exc} {logHint}')
+    except ConnectionAbortedError as exc:
+        raise MyError(f'recvEXC={exc} {logHint}')
+    if not data:
+        raise MyError(f'recvEOF {logHint}')
+    # if uBucket:
+    #     await uBucket.input(data)
+    return data
+
+async def aioWrite(w, data, *, logHint=''):
+    try:
+        w.write(data)
+        await w.drain()
+    except ConnectionAbortedError as exc:
+        raise MyError(f'sendEXC={exc} {logHint}')
+
+async def recieve_data_from_local(cl_reader, logHint=None, user=None):
+    try:
+        while True:
+            data = await aioRead(cl_reader, ReadMode.MAX, maxLen=65535, logHint='', bucket=uBucketdict[user])
+            print(data)
+    except MyError as exc:
+        log.info(f'{logHint} {exc}')
+
+async def send_data_to_server(cl_reader, rm_writer, logHint= None, user=None):
+    try:
+        uBucket = uBucketdict[user]
+        conn = sqlite3.connect('cache.db')
+        c = conn.cursor()
+        while True:
+            tokenNum = 65535
+            tokenNum = await uBucket.acquireToken(65535)
+            dps = await aioRead(cl_reader, ReadMode.MAX, maxLen=tokenNum, logHint='')
+            leftToken = tokenNum - len(dps)
+            if leftToken:
+                uBucket.releaseToken(leftToken)
+            await aioWrite(rm_writer, dps, logHint='')
+
+            await uBucket.lock.acquire()
+            c.execute("UPDATE USER SET BANDWITH=? WHERE USERNAME=?", (tokenNum, user,))
+            await uBucket.lock.release()
+
+    except MyError as exc:
+        log.info(f'{logHint} {exc}')
+
+async def transfer_client_remote(cl_reader, rm_writer, logHint=None, user=None):
+    try:
+        while True:
+            data = await aioRead(cl_reader, ReadMode.MAX, maxLen=65535, logHint='')
+            await aioWrite(rm_writer, data, logHint='')
+    except MyError as exc:
+        log.info(f'{logHint} {exc}')
+
+async def transfer_remote_client(rm_reader, cl_writer, logHint=None, user=None):
+    try:
+        while True:
+            data = await aioRead(rm_reader, ReadMode.MAX, maxLen=65535, logHint='recv data from server')
+            await aioWrite(cl_writer, data, logHint='')
+    except MyError as exc:
+        log.info(f'{logHint} {exc}')
+
+async def socks5ReadDstHost(r, atyp, *, logHint):
+    dstHost = None
+    if atyp == b'\x01':
+        dstHost = await aioRead(r, ReadMode.EXACT, exactLen=4, logHint=f'{logHint} ipv4')
+        dstHost = str(ipaddress.ip_address(dstHost))
+    elif atyp == b'\x03':
+        dataLen = await aioRead(r, ReadMode.EXACT, exactLen=1, logHint=f'{logHint} fqdnLen')
+        dataLen = dataLen[0]
+        dstHost = await aioRead(r, ReadMode.EXACT, exactLen=dataLen, logHint=f'{logHint} fqdn')
+        dstHost = dstHost.decode('utf8')
+    elif atyp == b'\x04':
+        dstHost = await aioRead(r, ReadMode.EXACT, exactLen=16, logHint=f'{logHint} ipv6')
+        dstHost = str(ipaddress.ip_address(dstHost))
+    else:
+        raise MyError(f'RECV ERRATYP={atyp} {logHint}')
+    return dstHost
+
+def socks5EncodeBindHost(bindHost):
+    atyp = b'\x03'
+    hostData = None
+    try:
+        ipAddr = ipaddress.ip_address(bindHost)
+        if ipAddr.version == 4:
+            atyp = b'\x01'
+            hostData = struct.pack('!L', int(ipAddr))
+        else:
+            atyp = b'\x04'
+            hostData = struct.pack('!16s', ipaddress.v6_int_to_packed(int(ipAddr)))
+    except Exception:
+        hostData = struct.pack(f'!B{len(bindHost)}s', len(bindHost), bindHost)
+    return atyp, hostData
+
+async def remoteProxyRun(reader, writer, logHint=None):
+    rm_reader, rm_writer = None, None
+    submit = await aioRead(reader, ReadMode.LINE)
+    if submit.decode().split(" ")[0] == "sumbit:":
+        params = submit.decode().split(" ")
+        username, password = params[1], params[2]
+        current_throughput = 0
+        async with aiosqlite.connect('cache.db') as db:
+            async with db.execute('SELECT * FROM USER WHERE USERNAME=? and PASSWORD=?', (username, password, ) ) as cur:
+                async for row in cur:
+                    current_throughput = row[2]
+                # print("now into database")
+                if current_throughput >= int(args.lim):
+                    print("server is too busy, please try to connect again after a while")
+                    return
+                version = await aioRead(reader, ReadMode.EXACT, exactLen=1, logHint=f'1stByte')
+                if b'\x05' == version:
+                    proxyType = 'SOCKS5'
+                    numMethods = await aioRead(reader, ReadMode.EXACT, exactLen=1, logHint='nMethod')
+                    await aioRead(reader, ReadMode.EXACT, exactLen=numMethods[0], logHint='methods')
+                    await aioWrite(writer, b'\x05\x00', logHint='method.noAuth')
+                    await aioRead(reader, ReadMode.EXACT, exactData=b'\x05\x01\x00', logHint='verCmdRsv')
+                    atyp = await aioRead(reader, ReadMode.EXACT, exactLen=1, logHint='atyp')
+                    dstHost = await socks5ReadDstHost(reader, atyp, logHint='dstHost')
+                    dstPort = await aioRead(reader, ReadMode.EXACT, exactLen=2, logHint='dstPort')
+                    dstPort = int.from_bytes(dstPort, 'big')
+
+
+                    rm_reader, rm_writer = await asyncio.open_connection(dstHost, dstPort)
+                    bindHost, bindPort, *_ = rm_writer.get_extra_info('sockname')
+
+                    atyp, hostData = socks5EncodeBindHost(bindHost)
+                    data = struct.pack(f'!ssss{len(hostData)}sH', b'\x05', b'\x00', b'\x00', atyp, hostData, int(bindPort))
+                    await aioWrite(writer, data, logHint='reply')
+
+                else:
+                    proxyType = 'HTTP TUNNEL'
+                    line = await aioRead(reader, ReadMode.LINE, logHint='Connection Request Header')
+                    line = version + line
+                    req_headers = await aioRead(reader, ReadMode.UNTIL, untilSep=b'\r\n\r\n',
+                                                logHint='Request Header')
+                    line = line.decode()
+                    method, uri, proto, *_ = line.split()
+
+                    if 'connect' == method.lower():
+                        proxyType = 'HTTPS'
+                        logHint = f'{logHint} {proxyType}'
+                        i = uri.find(':')
+                        if i:
+                            dstHost, dstPort = uri[:i], uri[i + 1:]
+                        else:
+                            dstHost, dstPort = uri, 8889
+                    else:
+                        raise MyError(f'RECV INVALID={line.strip()} EXPECT=CONNECT')
+
+                    logHint = f'{logHint} {dstHost} {dstPort}'
+                    log.info(f'{logHint} connStart...')
+                    try:
+                        rm_reader, rm_writer = await asyncio.open_connection(dstHost, int(dstPort))
+                        reply = f'HTTP/1.1 200 OK\r\n\r\n'
+                        await aioWrite(writer, reply.encode())
+                    except Exception as Err:
+                        MyError(Err)
+                        reply = "HTTP/1.1" + str(Err) + " Fail\r\n\r\n"
+                        await aioWrite(writer, reply.encode())
+
+                if rm_writer:
+                    print('call transfer data proc')
+                    await asyncio.wait({
+                        #asyncio.create_task(recieve_data_from_local(reader, user=username)),
+                        #asyncio.create_task(tokenLeakTask(username)),
+                        asyncio.create_task(send_data_to_server(reader, rm_writer, user=username)),
+                        # asyncio.create_task(recieve_data_from_server(rm_reader, user=username)),
+                        # asyncio.create_task(send_data_to_client(writer, user=username))
+                        # asyncio.create_task(transfer_client_remote(reader, rm_writer)),
+                        asyncio.create_task(transfer_remote_client(rm_reader, writer))
+                    })
+
+
+async def tokenLeakTask():
+    while True:
+        await asyncio.sleep(1)
+        for user, uBucket in uBucketdict.items():
+            uBucket.releaseToken(args.lim)
+
+async def remoteTask(port):
+    rm_srv = await asyncio.start_server(remoteProxyRun, host=remoteProxyHost, port=port)
+    addrList = list([s.getsockname() for s in rm_srv.sockets])
+    log.info(f'LISTEN Client Proxy {addrList}')
+    async with rm_srv:
+        await rm_srv.serve_forever()
+
+class leakyBucket(object):
+    def __init__(self, username,  capacity, leak_rate):
+        '''
+        :param capacity: the total data in the bucket
+        :param leak_rate: the rate data per seconds that the bucket leaks
+        :param :
+        '''
+        self.owner = username
+        self._capacity = float(capacity)
+        self._used_tokens = 0
+        self._leak_rate = leak_rate
+        self._last_time = 0
+        self.lock = asyncio.Lock()
+        self.tokenSemaphore = asyncio.BoundedSemaphore(1)
+        self._data = ""
+        self.status = True
+
+    def __del__(self):
+        self.lock = None
+        self.tokenSemaphore = None
+        self.data = None
+        self.owner = None
+
+    async def acquireToken(self, count):
+        await self.tokenSemaphore.acquire()
+        tokenCount = min(self._used_tokens, count)
+        self._used_tokens -= tokenCount
+        if self._used_tokens > 0:
+            try:
+                self.tokenSemaphore.release()
+            except ValueError:
+                pass
+        else:
+            self._used_tokens = 0
+        return tokenCount
+
+    def releaseToken(self, count):
+        # async with self.tokenLock:
+        self._used_tokens = min(self._used_tokens + int(count), self._capacity)
+        try:
+            self.tokenSemaphore.release()
+        except ValueError:
+            pass
+
+    async def input(self, data):
+        data = str(data)
+        newdatalen = len(data)
+        self.lock.acquire()
+        if len(self._data) + newdatalen < self._capacity:
+            self._data = self._data + data
+        else:
+            self._data = self._data + data[:self._capacity - len(self._data)]
+            print("data overflow out of the bucket {}" % data[self._capacity - len(self._data):])
+        self._last_time = time()
+        if len(data):
+            self.status = True
+        self.lock.release()
+
+    def write(self, lens):
+        lens = int(lens)
+        self.lock.acquire()
+        output = self._data[:lens] if (len(self._data) >= lens) else self._data
+        self._data = self._data[lens:] if (len(self._data) >= lens) else ""
+        if len(self._data) == 0:
+            self.status = False
+        self.lock.release()
+        return output
+
+
+def init_database():
+    conn = sqlite3.connect('cache.db')
+    c = conn.cursor()
+    c.execute('''DROP TABLE IF EXISTS USER ;''')
+    c.execute('''CREATE TABLE USER
+               (USERNAME           TEXT    NOT NULL,
+               PASSWORD           TEXT      ,
+               BANDWIDTH        DOUBLE      );''')
+    print("Table created successfully")
+
+    c.execute('''INSERT INTO USER(USERNAME, PASSWORD, BANDWIDTH) VALUES ('u1', '11', 0);''')
+    c.execute('''INSERT INTO USER(USERNAME, PASSWORD, BANDWIDTH) VALUES ('u2', '22', 0);''')
+    c.execute('''INSERT INTO USER(USERNAME, PASSWORD, BANDWIDTH) VALUES ('u3', '33', 0);''')
+
+
+    for user in ['u1', 'u2', 'u3']:
+        uBucketdict[user] = leakyBucket(user, args.cap, args.lim)
+        sBucketdict[user] = leakyBucket(user, args.cap, args.lim)
+
+    c.execute("SELECT * FROM USER")
+    for row in c:
+        print("NAME = ", row[0])
+        print("PASSWORD = ", row[1])
+        print("BANDWIDTH = ", row[2])
+
+    #('127.0.0.1', 8889)
+    c.execute('''CREATE TABLE INFO
+                (USERNAME           TEXT    NOT NULL,
+               PASSWORD           TEXT,
+               ADDRESS          TEXT,
+               PORT         TEXT,
+               STATUS       BOOL    NOT NULL);
+            ''')
+
+async def main():
+    init_database()
+    t1 = asyncio.create_task(remoteTask(remoteProxyPort))
+    t2 = asyncio.create_task(remoteTask(remotesocksPort))
+    t3 = asyncio.create_task(tokenLeakTask())
+
+    while True:
+        await asyncio.sleep(1)
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    _logFmt = logging.Formatter('%(asctime)s %(levelname).1s %(lineno)-3d %(funcName)-20s %(message)s', datefmt='%H:%M:%S')
+    _consoleHandler = logging.StreamHandler()
+    _consoleHandler.setLevel(logging.DEBUG)
+    _consoleHandler.setFormatter(_logFmt)
+
+    log = logging.getLogger(__file__)
+    log.addHandler(_consoleHandler)
+    log.setLevel(logging.DEBUG)
+
+    _parser = argparse.ArgumentParser(description='add traffic control')
+    _parser.add_argument('--limit', dest='lim', metavar='lim', default='1500000', help='the limit of the web ')
+    _parser.add_argument('--capacity', dest='cap', metavar='cap', default = '10000000', help='the capacity of the bucket')
+
+    args = _parser.parse_args()
+    asyncio.run(main())
